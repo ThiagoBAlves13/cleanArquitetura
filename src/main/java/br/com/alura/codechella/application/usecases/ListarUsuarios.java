fd8b18ddfaa0b6f527df8ad4574a866fedc594a3
Thiago Barreto Alves <thiagobalves13@gmail.com>
2025-07-03 15:31:51 -0300

    feat(listarTodosUsuarios)[aula_04]: criado listar todos os usuarios

---
+++ src/main/java/br/com/alura/codechella/application/usecases/ListarUsuarios.java
@@ -0,0 +1,19 @@
+package br.com.alura.codechella.application.usecases;
+
+import java.util.List;
+
+import br.com.alura.codechella.application.gateways.RepositorioDeUsuario;
+import br.com.alura.codechella.domain.entities.usuario.Usuario;
+
+public class ListarUsuarios {
+
+    private final RepositorioDeUsuario repositorioDeUsuario;
+
+    public ListarUsuarios(RepositorioDeUsuario repositorioDeUsuario){
+        this.repositorioDeUsuario = repositorioDeUsuario;
+    }
+
+    public List<Usuario> obterTodosUsuarios(){
+        return repositorioDeUsuario.listarTodos();
+    }
+}
